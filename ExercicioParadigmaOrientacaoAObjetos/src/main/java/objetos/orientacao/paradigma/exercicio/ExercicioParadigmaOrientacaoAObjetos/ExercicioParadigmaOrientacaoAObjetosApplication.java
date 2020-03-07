d38cc499498a70ee76cb8ed77e9e2a30c882ec2e
sinn-ss <sindel_santos@yahoo.com.br>
2020-03-07 00:59:04 -0300

    upando projeto

---
+++ ExercicioParadigmaOrientacaoAObjetos/src/main/java/objetos/orientacao/paradigma/exercicio/ExercicioParadigmaOrientacaoAObjetos/ExercicioParadigmaOrientacaoAObjetosApplication.java
@@ -0,0 +1,13 @@
+package objetos.orientacao.paradigma.exercicio.ExercicioParadigmaOrientacaoAObjetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioParadigmaOrientacaoAObjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioParadigmaOrientacaoAObjetosApplication.class, args);
+	}
+
+}
---
+++ ExercicioParadigmaOrientacaoAObjetos/src/test/java/objetos/orientacao/paradigma/exercicio/ExercicioParadigmaOrientacaoAObjetos/ExercicioParadigmaOrientacaoAObjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package objetos.orientacao.paradigma.exercicio.ExercicioParadigmaOrientacaoAObjetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioParadigmaOrientacaoAObjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
